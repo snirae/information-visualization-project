e7008afd1a0bd1841e136ab4700402a0453e4eb4
snirae <snir150401@gmail.com>
2023-06-22 22:30:07 +0300

    crimes sum line plot

--- final_vis.py
+++ final_vis.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 
 
-crimes_detailed = pd.read_csv("./data/cr_r_q_ft.csv")
-crimes_detailed["year"] = crimes_detailed['Quarter'].apply(lambda x: int(x.split("-")[0]))
+crimes_sum = pd.read_csv("./data/sum_cr_r_q.csv")
+crimes_sum["year"] = crimes_sum['Quarter'].apply(lambda x: int(x.split("-")[0]))
 
 st.header("Crime Records Visualization")
 
@@ -11,6 +11,6 @@ st.write("This is a web app to visualize crime records in the US.")
 
 # line plot for total crime records each year
 st.subheader("Total Crime Records Each Year")
-st.line_chart(crimes_detailed.groupby("year").size())
+st.line_chart(crimes_sum.groupby("year").size())
 
 # run the app with: streamlit run final_vis.py
