07d35aad2e857d5e25fd54befa92a627f97e775e
snirae <snir150401@gmail.com>
2023-06-22 23:52:47 +0300

    Update final_vis.py

--- final_vis.py
+++ final_vis.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import plotly.express as px
 import pandas as pd
 import numpy as np
+
+
+# run the app with: streamlit run final_vis.py
 
 
 crimes_sum = pd.read_csv("./data/sum_cr_r_q.csv", index_col=0)
@@ -11,10 +15,19 @@ sum_by_year = crimes_sum.groupby('year')['TikimSum'].sum()
 
 
 st.header("Crime Records Visualization")
-st.write("This is a web app to visualize crime records in Israel.")
+st.write("This is an app to visualize crime records in Israel.")
 
 # line plot for total TikimSum in each year, x axis is year, y axis is TikimSum
 st.subheader("Total Crime Records Each Year")
 st.line_chart(sum_by_year[sum_by_year.index != 2023])
 
-# run the app with: streamlit run final_vis.py
+
+crimes_det = pd.read_csv("./data/cr_r_q_ft.csv", index_col=0)
+felony_type = crimes_det['StatisticCrimeGroup'].value_counts(normalize=True).sort_values(ascending=False)
+
+# pie chart for felony type
+st.subheader("Felony Type")
+st.write("The most common felony type is", felony_type.index[0], "with", round(felony_type[0]*100, 2), "% of all crimes.")
+
+fig = px.pie(crimes_det, values=felony_type.values, names=felony_type.index, title='Felony Type')
+st.plotly_chart(fig)
