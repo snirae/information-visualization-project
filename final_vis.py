f8cbb221aa57d0ae462fd68ef249d57552b5115a
snirae <snir150401@gmail.com>
2023-06-22 23:25:49 +0300

    Update final_vis.py

--- final_vis.py
+++ final_vis.py
@@ -15,13 +15,6 @@ st.write("This is a web app to visualize crime records in Israel.")
 
 # line plot for total TikimSum in each year, x axis is year, y axis is TikimSum
 st.subheader("Total Crime Records Each Year")
-# st.line_chart(sum_by_year[sum_by_year.index != 2023])
-
-
-summary_by_year = crimes_sum.groupby('year')['TikimSum'].agg(['sum', 'mean']).reset_index()
-# Plotting
-st.title("Sum and Average of 'TikimSum' by Year (excluding 2023)")
-chart_data = pd.melt(summary_by_year, id_vars='Year', var_name='Metric', value_name='Value')
-st.line_chart(chart_data, use_container_width=True)
+st.line_chart(sum_by_year[sum_by_year.index != 2023])
 
 # run the app with: streamlit run final_vis.py
