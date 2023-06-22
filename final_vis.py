6fccda8ef9c4025b77c166ab4e925746fda58926
snirae <snir150401@gmail.com>
2023-06-22 23:25:07 +0300

    Update final_vis.py

--- final_vis.py
+++ final_vis.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import altair as alt
 import pandas as pd
 import numpy as np
 
@@ -17,27 +16,12 @@ st.write("This is a web app to visualize crime records in Israel.")
 # line plot for total TikimSum in each year, x axis is year, y axis is TikimSum
 st.subheader("Total Crime Records Each Year")
 # st.line_chart(sum_by_year[sum_by_year.index != 2023])
-summary_by_year = crimes_sum.groupby('year')['TikimSum'].agg(['sum', 'mean']).reset_index()
 
+
+summary_by_year = crimes_sum.groupby('year')['TikimSum'].agg(['sum', 'mean']).reset_index()
 # Plotting
 st.title("Sum and Average of 'TikimSum' by Year (excluding 2023)")
-chart = alt.Chart(summary_by_year).mark_line().encode(
-    x='Year',
-    y='sum',
-    color=alt.value('blue'),
-    tooltip=['Year', 'sum']
-).properties(
-    width=600,
-    height=400
-)
-
-chart += alt.Chart(summary_by_year).mark_line().encode(
-    x='Year',
-    y='mean',
-    color=alt.value('red'),
-    tooltip=['Year', 'mean']
-)
-
-st.altair_chart(chart)
+chart_data = pd.melt(summary_by_year, id_vars='Year', var_name='Metric', value_name='Value')
+st.line_chart(chart_data, use_container_width=True)
 
 # run the app with: streamlit run final_vis.py
