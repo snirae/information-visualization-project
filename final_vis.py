5f8e881d23ccd810d335a122362874ec2d4e88c1
snirae <snir150401@gmail.com>
2023-06-27 15:19:14 +0300

    Update final_vis.py

--- final_vis.py
+++ final_vis.py
@@ -17,7 +17,7 @@ crimes_sum = pd.read_csv("./data/sum_cr_r_q.csv", index_col=0)
 crimes_sum["year"] = crimes_sum['Quarter'].apply(lambda x: int(str(x).split("-")[0]) if not pd.isna(x) else x)
 crimes_sum['TikimSum'] = crimes_sum['TikimSum'].apply(lambda x: int(x.replace(',', '')) if not pd.isna(x) else x)
 
-sum_by_year = crimes_sum.groupby('year')['TikimSum'].sum()
+sum_by_year = crimes_sum.groupby('year')['TikimSum'].sum().reset_index()
 
 
 st.header("Crime Records Visualization")
