ce9275de66a0f06f43181ca02d6e528a75548154
snirae <snir150401@gmail.com>
2023-06-27 17:21:57 +0300

    Update final_vis.py

--- final_vis.py
+++ final_vis.py
@@ -14,6 +14,11 @@ from scipy import stats
 st.header("Crime Records Visualization")
 
 
+crimes_sum = pd.read_csv("./data/sum_cr_r_q.csv", index_col=0)
+crimes_sum["year"] = crimes_sum['Quarter'].apply(lambda x: int(str(x).split("-")[0]) if not pd.isna(x) else x)
+crimes_sum['TikimSum'] = crimes_sum['TikimSum'].apply(lambda x: int(x.replace(',', '')) if not pd.isna(x) else x)
+
+
 ########################################################################################################################
 
 
@@ -151,10 +156,6 @@ st.plotly_chart(fig)
 # line plot for total TikimSum in each year, x axis is year, y axis is TikimSum
 st.subheader("Total Crime Records Each Year")
 
-
-crimes_sum = pd.read_csv("./data/sum_cr_r_q.csv", index_col=0)
-crimes_sum["year"] = crimes_sum['Quarter'].apply(lambda x: int(str(x).split("-")[0]) if not pd.isna(x) else x)
-crimes_sum['TikimSum'] = crimes_sum['TikimSum'].apply(lambda x: int(x.replace(',', '')) if not pd.isna(x) else x)
 
 sum_by_year = crimes_sum.groupby('year')['TikimSum'].sum().reset_index()
 sum_by_year["Year"] = sum_by_year["year"]
