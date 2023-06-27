24e92bca0919c1a87124c2d7c48636ed13a62724
snirae <snir150401@gmail.com>
2023-06-27 15:44:03 +0300

    Update final_vis.py

--- final_vis.py
+++ final_vis.py
@@ -29,9 +29,15 @@ st.write("This is an app to visualize crime records in Israel.")
 st.subheader("Total Crime Records Each Year")
 fig = px.line(sum_by_year[sum_by_year['Year'] < 2023], x="Year", y="Total Crime Records", title='Total Crime Records / Year')
 fig.update_layout(
-    # plot_bgcolor='white',
-    paper_bgcolor='white',
-    font_color='black',
+    plot_bgcolor='white',
+    xaxis=dict(
+        showgrid=True,
+        gridcolor='black',
+    ),
+    yaxis=dict(
+        showgrid=True,
+        gridcolor='black',
+    ),
     font_size=20,
 )
 fig.update_traces(line_color='red')
